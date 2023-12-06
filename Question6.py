2d0528fc3d45fb2d49c2e6d1366e690c1a7cddc2
Priyansh Shah <Priyanshpokemon@gmail.com>
2023-12-05 21:23:15 -0800

    im bad holy broken hand though

--- Question6.py
+++ Question6.py
@@ -1,3 +1,17 @@
-inputs, *blocks = open('lines.txt','r+').read().split("\n\n")
-
-inputs = list(map(int, inputs.split(":")[1].split()))
+Time, Distance = open('lines.txt','r+').read().split("\n")
+t = Time.strip().split(":")[1]
+d = Distance.strip().split(":")[1]
+t = [int(x.strip()) for x in t.split(" ") if x.strip() != ""]
+d = [int(x.strip()) for x in d.split(" ") if x.strip() != ""]
+races = list(zip(t,d))
+raceswon = [0] * len(races)
+i = 0
+for race in races:
+    for time in range(race[0]):
+        if (race[0] - time) * time > race[1]:
+            raceswon[i] += 1
+    i += 1
+f = 0
+for race in raceswon:
+    f += race   
+print(f)
